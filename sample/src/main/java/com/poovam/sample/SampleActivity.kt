1e92f26e1acf52b8e7af4448d837a48ab7232680
Poovamraj T T <poovamraj@gmail.com>
2018-07-04 19:48:16 +0530

    sample added

--- sample/src/main/java/com/poovam/sample/SampleActivity.kt
+++ sample/src/main/java/com/poovam/sample/SampleActivity.kt
@@ -1,10 +1,11 @@
-package com.poovam.pinedittextfield
+package com.poovam.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.poovam.pinedittextfield.PinField
 
-class Sample : AppCompatActivity() {
+class SampleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,9 +13,10 @@ class Sample : AppCompatActivity() {
         val pinField = findViewById<PinField>(R.id.pinField)
         pinField.onTextCompleteListener = object : PinField.OnTextCompleteListener{
             override fun onTextComplete(enteredText: String) {
-                Toast.makeText(this@Sample,enteredText,Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@SampleActivity,enteredText, Toast.LENGTH_SHORT).show()
             }
 
         }
     }
 }
+
